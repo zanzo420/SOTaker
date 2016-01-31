ddc7ba74400ff7cfe58b77f31a7fb5e28d4f9214
Han Thi Nguyen <htn15@imperial.ic.ac.uk>
2016-01-31 14:32:27 +0000

    refactoring

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 import requests, json, bs4
 
-#TODO user input
-searchterm = "multiple inheritance java"
+def loadJson(url):
+    res = requests.get(url)
+    js = res.text
+    obj = json.loads(js)
+    return obj
 
-searchterm = searchterm.replace(' ', ';')
-searchurl = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=" + searchterm +\
+
+# TODO user input
+search_term = "async java".replace(' ', '+')
+searchurl = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=" + search_term + \
             "&answers=1&site=stackoverflow&filter=!mVN)3JfAau"
-res = requests.get(searchurl)
-js = res.text
-obj = json.loads(js)
+obj = loadJson(searchurl)
 answer_id = 0
 question_title = ""
 for i in range(len(obj["items"])):
     if "accepted_answer_id" in obj["items"][i]:
         answer_id = obj["items"][i]['accepted_answer_id']
         question_title = obj["items"][i]["title"]
-        break
 
-print(question_title)
-answerurl = "https://api.stackexchange.com/2.2/answers/" + str(answer_id) + \
+print(answer_id)
+print("//" + question_title)
+answer_url = "https://api.stackexchange.com/2.2/answers/" + str(answer_id) + \
             "?order=desc&sort=activity&site=stackoverflow&filter=!9YdnSMKKT"
-res = requests.get(answerurl)
-js = res.text
-obj = json.loads(js)
+obj = loadJson(answer_url)
 answer_body = obj["items"][0]["body"]
 soup = bs4.BeautifulSoup(answer_body, "html.parser")
 longest = 0
 best = ""
-for link in soup.find_all("code"):
-    if len(link.contents[0]) > longest:
-        longest = len(link.contents[0])
-        best = link.contents[0]
-print(best)
+if len(soup.find_all("code")) == 0:
+    print(soup.text)
+else:
+    # bias towards answers more towards the end
+    count = 1
+    for link in soup.find_all("code"):
+        code = link.contents[0]
+        length = len(code)
+        count += 0.1
+        if length * count > longest:
+            longest = length
+            best = code
+    print(best)
