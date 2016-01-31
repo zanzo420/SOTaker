3194899a89392beda91b1877aedccf095ed792d5
Han Thi Nguyen <htn15@imperial.ic.ac.uk>
2016-01-31 15:53:38 +0000

    improved searching for best answer

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-import requests, json, bs4
 import re
-import urllib.request, urllib.parse
+import urllib.parse
+import urllib.request
+
+import bs4
+import json
+import requests
+
 
 def loadJson(url):
     res = requests.get(url)
     js = res.text
-    obj = json.loads(js)
-    return obj
+    obj_ = json.loads(js)
+    return obj_
+
 
 def get_question_ids(searchfor):
-  query = urllib.parse.urlencode({'q': searchfor})
-  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
-  search_response = urllib.request.urlopen(url)
-  search_results = search_response.read().decode("utf8")
-  results = json.loads(search_results)
-  data = results['responseData']
-  hits = data['results']
-  ids = ""
-  for h in hits:
-      id = re.search(r"/(\d+)/", h["url"])
-      if id:
-          ids+=id.group(1) + ";"
-  return ids[:len(ids)-1]
+    query = urllib.parse.urlencode({'q': searchfor})
+    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+    search_response = urllib.request.urlopen(url)
+    search_results = search_response.read().decode("utf8")
+    results = json.loads(search_results)
+    data = results['responseData']
+    hits = data['results']
+    ids_ = ""
+    for h in hits:
+        id_ = re.search(r"/(\d+)/", h["url"])
+        if id_:
+            ids_ += id_.group(1) + ";"
+    return ids_[:len(ids_)-1]
 
 # TODO user input
-input = input("Search for: ")
-search_term = input.replace(' ', '+')
+input_ = input("Search for: ")
+search_term = input_.replace(' ', '+')
 ids = get_question_ids(search_term + " site:stackoverflow.com")
-#key for Stack Exchange API
-key = "ByWaOHYZMBBi5O4eNX6DyA((";
-searchurl = "https://api.stackexchange.com/2.2/questions/"+ids+"/answers?order=desc&sort=activity&site=stackoverflow&filter=!bJDus)chijK43X@key="+key
+# key for Stack Exchange API
+key = "ByWaOHYZMBBi5O4eNX6DyA(("
+searchurl = "https://api.stackexchange.com/2.2/questions/"+ids+\
+            "/answers?order=desc&sort=activity&site=stackoverflow&filter=!bJDus)chijK43X@key="+key
 obj = loadJson(searchurl)
 
 if "error_id" in obj:
@@ -39,17 +46,23 @@ if "error_id" in obj:
 
 score = -10000
 best_answer = ""
+highest_score_answer = ""
 for i in range(len(obj["items"])):
-    item = obj["items"][i]
-    body = item["body"]
-    if item["is_accepted"] == "False" or item["score"] < score:
+    if item["score"] < score:
         continue
+    item = obj["items"][i]
     score = item["score"]
-    best_answer = body
+    body = item["body"]
+    highest_score_answer = body
+    if item["is_accepted"] == "True":
+        best_answer = body
+
+if best_answer == "":
+    best_answer = highest_score_answer
 
 soup = bs4.BeautifulSoup(best_answer, "html.parser")
 longest = 0
-best = ""
+best_snippet = ""
 if len(soup.find_all("code")) == 0:
     print(soup.text)
 else:
@@ -63,5 +76,6 @@ else:
         count += 0.1
         if length * count > longest:
             longest = length
-            best = code
-    print(best)
+            best_snippet = code
+    print(best_snippet)
+
